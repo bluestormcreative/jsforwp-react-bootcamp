286735c694aaa8fd756c05a68b47efc55ce5bfdc
Max MacMillan <shannon@bluestormcreative.com>
2020-04-08 14:05:28 -0700

    Add my notes and questions

--- week3/statePractice/src/PracticeCounter.js
+++ week3/statePractice/src/PracticeCounter.js
@@ -1,18 +1,21 @@
 import React from 'react';
 
-const Header = (props) => {
+const Heading = (props) => {
+	// State becomes props here...
 	return <h2>{props.count}</h2>;
 };
 
 const Button = (props) => {
-	return <button onClick={props.onClick}>Increment me!</button>;
+	return <button onClick={props.onClick}>{props.buttonText}</button>;
 };
 
 const Counter = (props) => {
 	return (
 		<>
-			<Header count={props.count} />
-			<Button onClick={props.increment} />
+			<Heading count={props.count} />
+			<Button onClick={props.increment} buttonText='Increment Me!' />
+			<Button onClick={props.decrement} buttonText='Decrement Me!' />
+			<Button onClick={props.reset} buttonText='Reset Me!' />
 		</>
 	);
 };
@@ -23,13 +26,27 @@ export default class PracticeCounter extends React.Component {
 	};
 
 	incrementCount = () => {
+		// Why does 'this' work inside this arrow function?
 		this.setState({ count: this.state.count + 1 });
+	};
+
+	decrementCount = () => {
+		this.setState({ count: this.state.count - 1 });
+	};
+
+	resetCount = () => {
+		this.setState({ count: 0 });
 	};
 
 	render() {
 		return (
 			<>
-				<Counter count={this.state.count} increment={this.incrementCount} />
+				<Counter
+					count={this.state.count}
+					increment={this.incrementCount}
+					decrement={this.decrementCount}
+					reset={this.resetCount}
+				/>
 			</>
 		);
 	}
