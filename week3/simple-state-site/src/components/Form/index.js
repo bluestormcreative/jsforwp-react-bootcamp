1f5a9c1cad9f4dfb24e878793cbb5b047bc58874
Max MacMillan <shannon@bluestormcreative.com>
2020-04-08 20:15:38 -0700

    Add overlay and form component stubs

---
+++ week3/simple-state-site/src/components/Form/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default class Form extends React.Component {
+	render() {
+		return `I'm the form component`;
+	}
+}
---
+++ week3/simple-state-site/src/components/Overlay/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default class Overlay extends React.Component {
+	render() {
+		return `I'm the overlay`;
+	}
+}
