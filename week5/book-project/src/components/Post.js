48825c74832f286fbaa72954f5d0161aad965675
Max MacMillan <shannon@bluestormcreative.com>
2020-04-22 20:54:13 -0700

    Add quill-delta-to-html package and update post component

--- week5/book-project/src/components/Post.js
+++ week5/book-project/src/components/Post.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import { QuillDeltaToHtmlConverter } from 'quill-delta-to-html';
 
 // Destructure the post from props...
-const Post = ({ post }) => (
-	<article className='post container'>
-		<h1>{post.title}</h1>
-		<div>{post.content}</div>
-	</article>
-);
+const Post = ({ post }) => {
+	const converter = new QuillDeltaToHtmlConverter(post.content.ops, {});
+	const contentHTML = converter.convert();
+	return (
+		<article className='post container'>
+			<h1>{post.title}</h1>
+			<div
+				className='post-content'
+				dangerouslySetInnerHTML={{ __html: contentHTML }}
+			/>
+		</article>
+	);
+};
 export default Post;
