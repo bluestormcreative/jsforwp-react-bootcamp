ba19870891c0e7dfdfff674b336cc13324246d20
Max MacMillan <shannon@bluestormcreative.com>
2020-04-25 11:01:53 -0700

    Update post content to render with or without quill

--- week5/book-project/src/components/Post.js
+++ week5/book-project/src/components/Post.js
@@ -3,8 +3,13 @@ import { QuillDeltaToHtmlConverter } from 'quill-delta-to-html';
 
 // Destructure the post from props...
 const Post = ({ post }) => {
-	const converter = new QuillDeltaToHtmlConverter(post.content.ops, {});
-	const contentHTML = converter.convert();
+	let contentHTML = '';
+	if (post.content.ops) {
+		const converter = new QuillDeltaToHtmlConverter(post.content.ops, {});
+		contentHTML = converter.convert();
+	} else {
+		contentHTML = post.content;
+	}
 	return (
 		<article className='post container'>
 			<h1>{post.title}</h1>
