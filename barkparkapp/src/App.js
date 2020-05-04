c903cd967ea9e0e37990b65479dc63d719de2e64
Max MacMillan <max@alley.co>
2020-05-04 15:49:05 -0700

    Set up some default calendar props for the view we want

--- barkparkapp/src/App.js
+++ barkparkapp/src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component } from 'react';
+import { Calendar, momentLocalizer } from "react-big-calendar";
+import moment from 'moment';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const localizer = momentLocalizer(moment);
+
+class App extends Component {
+    state = {
+      events: [
+        // {
+        //   start: moment().toDate(),
+        //   end: moment()
+        //     .add(1, "days")
+        //     .toDate(),
+        //   title: "Some title"
+        // }
+      ]
+  };
+  render() {
+    return (
+      <div className="App">
+        <div className="calendar__container">
+          <h2 className="calendar__heading">Bark Park Calendar</h2>
+          <Calendar
+            localizer={localizer}
+            defaultDate={new Date()}
+            defaultView="week"
+            events={this.state.events}
+            style={{ height: "100vh" }}
+            selectable={true}
+            showMultiDayTimes={true}
+            views={['week', 'day']}
+            min={moment('06:00am', 'h:mma').toDate()}
+            max={moment('09:00pm', 'h:mma').toDate()}
+          />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
