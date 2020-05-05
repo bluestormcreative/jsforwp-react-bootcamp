6695acb6650014accc1e14601a3fecd20de8c998
Max MacMillan <shannon@bluestormcreative.com>
2020-05-05 10:26:31 -0700

    Import firebase and login componentss, pass simple login function

--- barkparkapp/src/App.js
+++ barkparkapp/src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
-import { Calendar, momentLocalizer } from "react-big-calendar";
+import firebase from './firebase';
+import Login from './components/Login';
+import { Calendar, momentLocalizer } from 'react-big-calendar';
 import moment from 'moment';
 import { v4 as uuidv4 } from 'uuid';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
@@ -15,20 +17,24 @@ class App extends Component {
 			userName: 'Mo',
 			userEmail: 'mo@twobigdogs.us',
 			userPhone: '665-567-7890',
-			petNames: [
-				'Leo',
-				'Lucy',
-			],
+			petNames: ['Leo', 'Lucy'],
 			reservedSlots: [],
 			availSlots: 3,
 		},
+		isAuthenticated: false,
 	};
 
-	handleSelect = ({ slots, start, end }) => { // Destructured from slotInfo.
-		const {
-			events,
-			userData,
-		} = this.state;
+	onLogin = (userEmail, userPass) => {
+		firebase
+			.auth()
+			.signInWithEmailAndPassword(userEmail, userPass)
+			.then((user) => this.setState({ isAuthenticated: true }))
+			.catch((error) => console.error(error));
+	};
+
+	handleSelect = ({ slots, start, end }) => {
+		// Destructured from slotInfo.
+		const { events, userData } = this.state;
 
 		const now = new Date();
 
@@ -58,8 +64,10 @@ class App extends Component {
 		}
 
 		// Return if user has already reserved all their available slots.
-		if (userData.reservedSlots.length > userData.availSlots - 1 ) {
-			alert(`Sorry, you've already reserved ${userData.availSlots} timeslots this week!`);
+		if (userData.reservedSlots.length > userData.availSlots - 1) {
+			alert(
+				`Sorry, you've already reserved ${userData.availSlots} timeslots this week!`
+			);
 			return;
 		}
 
@@ -77,7 +85,7 @@ class App extends Component {
 							start,
 							end,
 							title,
-							slotID
+							slotID,
 						},
 					],
 				},
@@ -89,57 +97,80 @@ class App extends Component {
 						start,
 						end,
 						title,
-						slotID
+						slotID,
 					},
 				],
 			});
 		}
-	}
+	};
 
 	render() {
-		const remainingSlots = this.state.userData.availSlots - this.state.userData.reservedSlots.length;
+		const remainingSlots =
+			this.state.userData.availSlots -
+			this.state.userData.reservedSlots.length;
+
 		let currentSlots = this.state.userData.reservedSlots.map((obj) => {
 			const dayDate = moment(obj.start).format('ddd MMM Do');
 			const startTime = moment(obj.start).format('h:mma');
 			const endTime = moment(obj.end).format('h:mma');
-			return <li><span>{dayDate}:</span><span>{startTime} - {endTime}</span></li>;
+			return (
+				<li>
+					<span>{dayDate}:</span>
+					<span>
+						{startTime} - {endTime}
+					</span>
+				</li>
+			);
 		});
 
-		if (currentSlots.length === 0 ) {
-			currentSlots = <li className='no-slots'>You haven't reserved any timeslots yet!</li>;
+		if (currentSlots.length === 0) {
+			currentSlots = (
+				<li className='no-slots'>
+					You haven't reserved any timeslots yet!
+				</li>
+			);
 		}
 
 		return (
-			<div className="App">
-				<div className="calendar__container">
-					<div className="calendar__header">
-						<div>
-							<h2 className="calendar__heading">Bark Park Calendar</h2>
-							<p className="calendar__description">Click on a 30min timeslot to reserve your spot!</p>
+			<div className='App'>
+				{!this.state.isAuthenticated && (
+					<Login onLogin={this.onLogin} />
+				)}
+				{this.state.isAuthenticated && (
+					<div className='calendar__container'>
+						<div className='calendar__header'>
+							<div>
+								<h2 className='calendar__heading'>
+									Bark Park Calendar
+								</h2>
+								<p className='calendar__description'>
+									Click on a 30min timeslot to reserve your
+									spot!
+								</p>
+							</div>
+							<div>
+								<p>Slots available: {remainingSlots}</p>
+								<p>
+									Your reserved timeslots:
+									<ul>{currentSlots}</ul>
+								</p>
+							</div>
 						</div>
-						<div>
-							<p>Slots available: {remainingSlots}</p>
-							<p>Your reserved timeslots:
-								<ul>
-										{currentSlots}
-								</ul>
-							</p>
-						</div>
+						<Calendar
+							localizer={localizer}
+							defaultDate={new Date()}
+							defaultView='week'
+							events={this.state.events}
+							onSelectSlot={this.handleSelect}
+							selectable
+							step={30}
+							showMultiDayTimes
+							views={['week']}
+							min={moment('06:00am', 'h:mma').toDate()}
+							max={moment('09:00pm', 'h:mma').toDate()}
+						/>
 					</div>
-					<Calendar
-						localizer={localizer}
-						defaultDate={new Date()}
-						defaultView="week"
-						events={this.state.events}
-						onSelectSlot={this.handleSelect}
-						selectable
-						step={30}
-						showMultiDayTimes
-						views={['week']}
-						min={moment('06:00am', 'h:mma').toDate()}
-						max={moment('09:00pm', 'h:mma').toDate()}
-					/>
-				</div>
+				)}
 			</div>
 		);
 	}
