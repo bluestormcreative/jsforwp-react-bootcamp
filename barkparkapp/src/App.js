4b68dbe4305ab525bbb906223ade78609d35a3d4
Max MacMillan <shannon@bluestormcreative.com>
2020-05-05 10:33:23 -0700

    Import SimpleStorage component and sent App as parent

--- barkparkapp/src/App.js
+++ barkparkapp/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import SimpleStorage from 'react-simple-storage';
 import firebase from './firebase';
 import Login from './components/Login';
 import { Calendar, momentLocalizer } from 'react-big-calendar';
@@ -133,6 +134,7 @@ class App extends Component {
 
 		return (
 			<div className='App'>
+				<SimpleStorage parent={this} />
 				{!this.state.isAuthenticated && (
 					<Login onLogin={this.onLogin} />
 				)}
