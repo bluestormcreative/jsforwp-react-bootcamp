3e16f3a1f9bc754f7da376c514ea6c1cf7315806
Max MacMillan <max@alley.co>
2020-05-04 21:08:28 -0700

    Add dummy state, set up selection handler to alert if invalid

--- barkparkapp/src/App.js
+++ barkparkapp/src/App.js
@@ -1,4 +1,4 @@
-import React, {Component } from 'react';
+import React, { Component } from 'react';
 import { Calendar, momentLocalizer } from "react-big-calendar";
 import moment from 'moment';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
@@ -7,38 +7,82 @@ import './App.css';
 const localizer = momentLocalizer(moment);
 
 class App extends Component {
-    state = {
-      events: [
-        // {
-        //   start: moment().toDate(),
-        //   end: moment()
-        //     .add(1, "days")
-        //     .toDate(),
-        //   title: "Some title"
-        // }
-      ]
-  };
-  render() {
-    return (
-      <div className="App">
-        <div className="calendar__container">
-          <h2 className="calendar__heading">Bark Park Calendar</h2>
-          <Calendar
-            localizer={localizer}
-            defaultDate={new Date()}
-            defaultView="week"
-            events={this.state.events}
-            style={{ height: "100vh" }}
-            selectable={true}
-            showMultiDayTimes={true}
-            views={['week', 'day']}
-            min={moment('06:00am', 'h:mma').toDate()}
-            max={moment('09:00pm', 'h:mma').toDate()}
-          />
-        </div>
-      </div>
-    );
-  }
+	state = {
+		events: [],
+		userData: {
+			id: 489,
+			petNames: [
+				'Leo',
+				'Lucy',
+			],
+			userName: 'Mo',
+			reservedSlots: [],
+		},
+	};
+
+	handleSelect = ({ slots, start, end }) => {
+		console.log(this.state.userData.reservedSlots.length); // eslint-disable-line no-console
+
+		if (slots.length > 2) {
+			alert("Sorry, only 30 minute slots allowed!");
+			return;
+		}
+
+		if (this.state.userData.reservedSlots.length > 2 ) {
+			alert("Sorry, you've already reserved 3 timeslots this week!");
+			return;
+		}
+
+		const title = `${this.state.userData.userName} & ${this.state.userData.petNames[0]}`; // This can be the username by default.
+		// Then just a click "Yes" or "Cancel", then if "Yes"...
+		if (title)
+			this.setState({
+				userData: {
+					...this.state.userData,
+					reservedSlots: [
+						...this.state.userData.reservedSlots,
+						{start,
+						end,
+						title,},
+					],
+				},
+			});
+			this.setState({
+				events: [
+					...this.state.events,
+					{
+						start,
+						end,
+						title,
+					},
+				],
+			});
+	}
+
+	render() {
+		return (
+			<div className="App">
+				<div className="calendar__container">
+					<h2 className="calendar__heading">Bark Park Calendar</h2>
+					<p className="calendar__description">Click on a 30min timeslot to reserve your spot!</p>
+					<Calendar
+						localizer={localizer}
+						defaultDate={new Date()}
+						defaultView="week"
+						events={this.state.events}
+						onSelectSlot={this.handleSelect}
+						style={{ height: "100vh" }}
+						selectable
+						step={30}
+						showMultiDayTimes
+						views={['week', 'day']}
+						min={moment('06:00am', 'h:mma').toDate()}
+						max={moment('09:00pm', 'h:mma').toDate()}
+					/>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default App;
