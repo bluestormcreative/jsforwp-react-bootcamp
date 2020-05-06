4de93cd4f7dcae3c8fad8531a6f4bacbdbd1c0ff
Max MacMillan <shannon@bluestormcreative.com>
2020-05-05 21:02:56 -0700

    Add a heading to the login form

--- barkparkapp/src/components/Login.js
+++ barkparkapp/src/components/Login.js
@@ -13,41 +13,41 @@ export default class Login extends Component {
 
 	render() {
 		return (
-			<form
-				className='App__login'
-				name='login'
-				onSubmit={this.handleLogin}>
-				<p>
-					<label htmlFor='userEmail'>Email:</label>
-					<input
-						name='userEmail'
-						type='email'
-						onChange={(event) =>
-							this.setState({ userEmail: event.target.value })
-						}
-					/>
-				</p>
-				<p>
-					<label htmlFor='userPass'>Password:</label>
-					<input
-						name='userPass'
-						type='password'
-						onChange={(event) =>
-							this.setState({ userPass: event.target.value })
-						}
-					/>
-				</p>
-				<p>
-					<button
-						className='btn btn--login'
-						type='submit'
-						disabled={
-							!this.state.userEmail && !this.state.userPass
-						}>
-						Login
-					</button>
-				</p>
-			</form>
+			<div className='App__login'>
+				<h2>Schedule Time at the Bark Park</h2>
+				<form name='login' onSubmit={this.handleLogin}>
+					<p>
+						<label htmlFor='userEmail'>Email:</label>
+						<input
+							name='userEmail'
+							type='email'
+							onChange={(event) =>
+								this.setState({ userEmail: event.target.value })
+							}
+						/>
+					</p>
+					<p>
+						<label htmlFor='userPass'>Password:</label>
+						<input
+							name='userPass'
+							type='password'
+							onChange={(event) =>
+								this.setState({ userPass: event.target.value })
+							}
+						/>
+					</p>
+					<p>
+						<button
+							className='btn btn--login'
+							type='submit'
+							disabled={
+								!this.state.userEmail && !this.state.userPass
+							}>
+							Login
+						</button>
+					</p>
+				</form>
+			</div>
 		);
 	}
 }
