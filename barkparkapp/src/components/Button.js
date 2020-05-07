615a2af9e0f673b31fb1b8998b746ad0cdde0c0b
Max MacMillan <shannon@bluestormcreative.com>
2020-05-07 00:34:31 -0700

    Prettier

--- barkparkapp/src/components/Button.js
+++ barkparkapp/src/components/Button.js
@@ -1,13 +1,10 @@
 import React from 'react';
 
 const Button = (props) => {
-    return(
-        <button
-            className={props.className}
-            onClick={props.onClick}
-        >
-            {props.text}
-        </button>
-    );
+	return (
+		<button className={props.className} onClick={props.onClick}>
+			{props.text}
+		</button>
+	);
 };
 export default Button;
--- barkparkapp/src/components/UserSlotsList.js
+++ barkparkapp/src/components/UserSlotsList.js
@@ -1,18 +1,16 @@
 import React from 'react';
 
 const UserSlotsList = (props) => {
+	const { currentSlots, remainingSlots } = props;
 
-    const {
-        currentSlots,
-        remainingSlots
-    } = props;
-
-    return(
-        <div className='userslots'>
-            <p className='userslots__remaining'>Timeslots available: {remainingSlots}</p>
-            <p className='userslots__reserved'>Your reserved timeslots:</p>
-            <ol className='userslots__list'>{currentSlots}</ol>
-        </div>
-    );
-}
+	return (
+		<div className='userslots'>
+			<p className='userslots__remaining'>
+				Timeslots available: {remainingSlots}
+			</p>
+			<p className='userslots__reserved'>Your reserved timeslots:</p>
+			<ol className='userslots__list'>{currentSlots}</ol>
+		</div>
+	);
+};
 export default UserSlotsList;
